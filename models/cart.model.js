21b92f4df5f4d36c67cbd5044609e96f96e29498
Arpan Mondal <arpan19991025@gmail.com>
2021-10-06 23:55:02 +0530

    Update cart and products model

--- models/cart.model.js
+++ models/cart.model.js
@@ -3,13 +3,11 @@ const { Schema, model } = mongoose
 
 const cartSchema = new Schema({
     addressId : { type : Schema.Types.ObjectId, ref : "address", default : null },
-    userId    : { type : Schema.Types.ObjectId, ref : "user" },
+    userId    : { type : Schema.Types.ObjectId, ref : "user", required : "UserId is required" },
     products  : [ 
         { 
-            color     : { type : String },
             productId : { type : Schema.Types.ObjectId, ref : "product" },
-            quantity  : { type : Number, required : "Quantity is required", default : 1},
-            size      : { type : String, required : "Size is required" },
+            quantity  : { type : Number, default : 1},
         } 
     ]
 })
--- models/products.model.js
+++ models/products.model.js
@@ -4,8 +4,7 @@ const { Schema, model } = mongoose
 const ProductSchema = new Schema({
     availableQty : { type : Number, required : "Available quantity is required" },
     brand        : { type : String, required : "Product Brand is required" },
-    cateory      : { type : String, required : "Product category is required" },
-    colors       : { type : [ String ] },
+    category      : { type : String, required : "Product category is required" },
     discount     : { type : Number , default : 0 },       
     fastDelivery : { type : Boolean, required : "Fast delivery option is required" },
     image        : { type : String, required : "Product image is required" },
@@ -15,7 +14,6 @@ const ProductSchema = new Schema({
     oldPrice     : { type : Number },
     price        : { type : Number, required : "Product price is required" },
     rating       : { stars : { type : Number, default : 0 } },
-    sizes        : { type : [ String ] },
     url          : { type : String },
 })
 
